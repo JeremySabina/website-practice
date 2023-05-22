233ba3e9d68b8e49a77ac6976a16d22a9e66b90f
Jeremy Sabina <Jeremy_Sabina@yahoo.com>
2023-05-22 01:25:16 -0400

    Added css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Initialize modules
-const { src, dest, watch, series } = require('gulp');
+
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -9,22 +10,22 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
-//sass.compiler = require('dart-sass');
+// sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
-  return src('app/scss/style.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest('dist', { sourcemaps: '.' }));
+  return src('app/scss/style.scss', {sourcemaps : true})
+  .pipe(sass())
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(dest('dist', {sourcemaps: '.'}));
 }
 
 // JavaScript Task
 function jsTask() {
-  return src('app/js/script.js', { sourcemaps: true })
-    .pipe(babel({ presets: ['@babel/preset-env'] }))
-    .pipe(terser())
-    .pipe(dest('dist', { sourcemaps: '.' }));
+  return src('app/js/script.js', {sourcemaps : true})
+  .pipe(babel({presets: ['@babel/preset-env']}))
+  .pipe(terser())
+  .pipe(dest('dist', {sourcemaps: '.'}));
 }
 
 // Browsersync
@@ -42,22 +43,20 @@ function browserSyncServe(cb) {
   });
   cb();
 }
-function browserSyncReload(cb) {
+
+function browserSyncReLoad(cb) {
   browsersync.reload();
   cb();
 }
 
 // Watch Task
 function watchTask() {
-  watch('*.html', browserSyncReload);
+  watch('*.html', browserSyncReLoad);
   watch(
     ['app/scss/**/*.scss', 'app/**/*.js'],
-    series(scssTask, jsTask, browserSyncReload)
+    series(scssTask, jsTask, browserSyncReLoad)
   );
 }
 
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
-
-// Build Gulp Task
-exports.build = series(scssTask, jsTask);
